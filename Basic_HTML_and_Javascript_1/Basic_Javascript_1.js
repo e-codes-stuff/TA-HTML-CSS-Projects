b621dc51d04c5a064ea2a132ee0a60a04820290b
e-codes-stuff <110307781+e-codes-stuff@users.noreply.github.com>
2023-09-20 12:47:10 -0700

    Complete basic HTML and JS assignments

---
+++ Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
@@ -0,0 +1,10 @@
+function send_alert() {
+    alert("Hello, World! I am an alert box!");
+}
+
+function My_First_Function() {
+    var String = "Kiss me, I'm Irish!";
+    var result = String.fontcolor("green");
+
+    document.getElementById("Irish").innerHTML = result;
+}
