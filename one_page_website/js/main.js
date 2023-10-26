55ec1f35f82fdb7a2ed233d5312932cae00554e9
e-codes-stuff <110307781+e-codes-stuff@users.noreply.github.com>
2023-10-26 13:21:56 -0700

    Update modal

--- one_page_website/js/main.js
+++ one_page_website/js/main.js
@@ -1,15 +1,46 @@
-// JavaScript functions for opening and closing the lightbox
-function openLightbox(imageSrc, caption) {
-    var modal = document.getElementById("myModal");
-    var modalImage = document.getElementById("modal-image");
-    var captionText = document.getElementById("caption");
+var slideIndex = 1;
 
-    modal.style.display = "block";
-    modalImage.src = imageSrc;
-    captionText.innerHTML = caption;
+function openLightbox(n) {
+    showSlides(slideIndex = n);
 }
 
-function closeLightbox() {
+function closeModal() {
     var modal = document.getElementById("myModal");
     modal.style.display = "none";
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
 }
